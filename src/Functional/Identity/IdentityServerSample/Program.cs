26cb86d9f66c0e337f58ba115e5a277d8392d30b
hdh <egman@qq.com>
2020-06-19 14:57:08 +0800

    写注释

--- src/Functional/Identity/IdentityServerSample/Program.cs
+++ src/Functional/Identity/IdentityServerSample/Program.cs
@@ -24,28 +24,30 @@ namespace IdentityServerSample
         {
             var builder = new WebHostBuilder();
 
-            if (string.IsNullOrEmpty(builder.GetSetting(WebHostDefaults.ContentRootKey)))
+            if (string.IsNullOrEmpty(builder.GetSetting(WebHostDefaults.ContentRootKey)))  //"contentRoot"
             {
-                builder.UseContentRoot(Directory.GetCurrentDirectory());
+                builder.UseContentRoot(Directory.GetCurrentDirectory());//当前目录
             }
             if (args != null)
             {
-                builder.UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build());
+                builder.UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build()); //附加配置文件
             }
 
+            //使用小的host服务器 kestrel
             builder.UseKestrel((builderContext, options) =>
             {
-                options.Configure(builderContext.Configuration.GetSection("Kestrel"));
+                options.Configure(builderContext.Configuration.GetSection("Kestrel")); //配置kestrel
             })
-            .ConfigureAppConfiguration((hostingContext, config) =>
+            .ConfigureAppConfiguration((hostingContext, config) => //配置应用程序
             {
-                var env = hostingContext.HostingEnvironment;
+                var env = hostingContext.HostingEnvironment; //host的环境
 
                 config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true); //加载配置文件
 
                 if (env.IsDevelopment())
                 {
+                    //调试模式时, 使用当前环境 程序集
                     var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
                     if (appAssembly != null)
                     {
@@ -53,22 +55,23 @@ namespace IdentityServerSample
                     }
                 }
 
-                config.AddEnvironmentVariables();
+                config.AddEnvironmentVariables(); //使用环境变量
 
                 if (args != null)
                 {
-                    config.AddCommandLine(args);
+                    config.AddCommandLine(args); //配置命令行
                 }
             })
             .ConfigureLogging((hostingContext, logging) =>
             {
-                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging")).AddConsole().AddDebug();
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging")).AddConsole().AddDebug(); //使用日志,配置日志信息
             })
             .ConfigureServices((hostingContext, services) =>
             {
-                // Fallback
+                // Fallback  应急计划
                 services.PostConfigure<HostFilteringOptions>(options =>
                     {
+                        //使用白名单
                         if (options.AllowedHosts == null || options.AllowedHosts.Count == 0)
                         {
                             // "AllowedHosts": "localhost;127.0.0.1;[::1]"
@@ -77,15 +80,16 @@ namespace IdentityServerSample
                             options.AllowedHosts = (hosts?.Length > 0 ? hosts : new[] { "*" });
                         }
                     });
-                // Change notification
+                
+                // Change notification 改变通知?
                 services.AddSingleton<IOptionsChangeTokenSource<HostFilteringOptions>>(
                     new ConfigurationChangeTokenSource<HostFilteringOptions>(hostingContext.Configuration));
 
-                services.AddTransient<IStartupFilter, HostFilteringStartupFilter>();
+                services.AddTransient<IStartupFilter, HostFilteringStartupFilter>(); //启动过滤器
             })
             .UseDefaultServiceProvider((context, options) =>
             {
-                options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                options.ValidateScopes = context.HostingEnvironment.IsDevelopment(); //验证作用域?
             });
             return builder;
         }
